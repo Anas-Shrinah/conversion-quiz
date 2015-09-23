661bfc5bbb3754e9038ac84e1d72f0ed8535e755
Steve Kerrison <git@stevekerrison.com>
2015-09-23 16:27:54 +0100

    Works in Chrome

--- quiz.js
+++ quiz.js
@@ -14,7 +14,7 @@ function NBQuiz() {
         'simple': {
             'questions':    20,
             'bases':        [2, 10],
-            'pow':          4,
+            'pow':          4, // Unsigned: 0--2^(pow)-1, signed: -2^(pow-1) -- 2^(pow-1)-1
             'signed':       false
         },
         'medium': {
@@ -44,7 +44,7 @@ function NBQuiz() {
     this.toBaseString = function (v, b) {
         var s = '';
         if (b !== 10) {
-            var ndigits = (Math.pow(2, this.config['pow']) - 1).toString(b).length;
+            var ndigits = (Math.pow(2, this.config.pow) - 1).toString(b).length;
             s = (v >>> 0).toString(b);
             s = String("0".repeat(ndigits) + s).slice(-ndigits);
             
@@ -55,11 +55,12 @@ function NBQuiz() {
     };
     
     this.answer = function () {
-        $('form#quiz input[type=submit]').val('Continue').focus();
         $('#conversion').prop('disabled', true);
         $('form#quiz').off('submit').submit($.proxy(this.nextq, this));
         
         this.total += 1;
+        
+        /* Ensure guess is formatted as requested */
         var guess = $('#conversion').val();
         var answer = this.toBaseString(this.value, this.to);
         if (guess === answer) {
@@ -71,27 +72,40 @@ function NBQuiz() {
         $('#correct').text(this.score);
         $('#total').text(this.total);
         $('#progress').show();
+        
+        /* Deal with quiz end or continuous quizzing */
+        if (this.total === this.config.questions || this.config.questions === 0) {
+            this.perct = (this.score / this.total * 100.0).toFixed(2);
+            $('#percentage span').text(this.perct);
+            $('#percentage').show();
+        }
+        if (this.total === this.config.questions) {
+            $('form#quiz input[type=submit]').val('Restart').focus();
+            $('form#quiz').off('submit').submit($.proxy(this.init, this));
+        } else {
+            $('form#quiz input[type=submit]').val('Continue').focus();
+        }
         return false;
     };
     
     this.nextq = function () {
         /* Calculate the value we want to use from the quiz mode */
-        this.base = this.choose(this.config['bases']);
-        this.value = Math.floor(Math.random() * Math.pow(2, this.config['pow']));
+        this.base = this.choose(this.config.bases);
+        this.value = Math.floor(Math.random() * Math.pow(2, this.config.pow));
         this.signed = false;
         this.currq += 1;
-        if (this.config['signed'] === true && Math.random() > 0.5) {
+        if (this.config.signed === true && Math.random() > 0.5) {
             this.signed = true;
-            this.value -= Math.pow(2, this.config['pow'] - 1);
+            this.value -= Math.pow(2, this.config.pow - 1);
         }
         this.to = this.base;
         do {
-            this.to = this.choose( this.config['bases'] );
+            this.to = this.choose( this.config.bases );
         } while (this.to === this.base); //Ugh
         if (this.to == 10) {
             this.ndigits = this.toBaseString(this.value, this.to).length;
         } else {
-            this.ndigits = (Math.pow(2, this.config['pow']) - 1).toString(this.to).length;
+            this.ndigits = (Math.pow(2, this.config.pow) - 1).toString(this.to).length;
         }
         
         /* Setup the content */
@@ -100,7 +114,7 @@ function NBQuiz() {
         $('#base').text(this.to);
         $('#ndigit').text(this.ndigits);
         $('#qno').text(this.currq);
-        $('#qof').text(this.config['questions'] > 0 ? ' of ' + this.config['questions'] : '');
+        $('#qof').text(this.config.questions > 0 ? ' of ' + this.config.questions : '');
         if (this.to === 10) {
             $('#sign').text( this.signed ? 'signed (negative symbol where needed)' : 'unsigned');
         } else {
